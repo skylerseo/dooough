55bba404dd8d55d87143087e3a0f4f7c29e85247
Skyler Seo <skyler@Skylerui-MacBookPro.local>
2021-12-26 23:09:13 +0900

    update five jjal

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
-import React from 'react'
 import './App.css'
 
-function App() {
-  return <></>
+import data from './Main/data.json'
+
+const App = (): JSX.Element => {
+  return (
+    <ul>
+      {data.list.map((d, i) => {
+        const key = `data-${i}`
+        return (
+          <li key={key}>
+            <p className='title'>{d.title}</p>
+            <img src={`/images/jjal/${d.url}`} alt='' />
+          </li>
+        )
+      })}
+    </ul>
+  )
 }
 
 export default App
