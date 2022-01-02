56de09498d4f8a75f33d0fb8e779f2749f5d5f82
Skyler Seo <skyler@Skylerui-MacBookPro.local>
2022-01-02 22:56:24 +0900

    modified all emoticon title and tag

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
-import './App.css'
-
+import './App.scss'
 import data from './Main/data.json'
 
 const App = (): JSX.Element => {
   return (
-    <ul>
-      {data.list.map((d, i) => {
-        const key = `data-${i}`
-        return (
-          <li key={key}>
-            <p className='title'>{d.title}</p>
-            <img src={`/images/jjal/${d.url}`} alt={d.title} />
-          </li>
-        )
-      })}
-    </ul>
+    <>
+      <input type='text' placeholder='검색어를 입력해주세요' />
+      <button type='button'>검색</button>
+      <ul>
+        {data.list.map((d, i) => {
+          const key = `data-${i}`
+          return (
+            <li key={key}>
+              <p className='title'>{d.title}</p>
+              <img src={`/images/jjal/${d.url}`} alt={d.title} />
+              <p className='tag'>{d.tags.map((tag) => `#${tag} `)}</p>
+            </li>
+          )
+        })}
+      </ul>
+    </>
   )
 }
 
